5a5932b12b8015807961bbdcff7bc788f02fe58a
Adam <adam.moftah@gmail.com>
2020-05-20 01:50:55 -0400

    finish styling and add new demo song

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./styles/index.scss";
 
 let indexVariable = 50;
 const graph = document.getElementById("visual");
+const modal = document.getElementById("modal");
+const infoLink = document.getElementById("info-link");
 setInterval(function () {
   indexVariable = (++indexVariable % 255) + Math.random() * 50 + 20;
 }, 750);
@@ -13,7 +15,7 @@ function startVisualizer() {
   const audioElement = document.getElementById("audioElement");
   const audioSrc = audioCtx.createMediaElementSource(audioElement);
   analyser = audioCtx.createAnalyser();
-  frequencyData = new Uint8Array(indexVariable);
+  frequencyData = new Uint8Array(300);
 
   createSvg();
   // Bind our analyser to the media element source.
@@ -25,13 +27,13 @@ function startVisualizer() {
 }
 
 window.onload = function () {
-  setDemoSong();
+  setDemoSong1();
   startVisualizer();
 };
 
 function createSvg() {
-  const svgHeight = (window.innerHeight / 5) * 3;
-  const svgWidth = window.innerWidth - 100;
+  const svgHeight = window.innerHeight * 0.88;
+  const svgWidth = window.innerWidth;
   // const barPadding = "0";
   // d3.selectAll("svg").remove();
   svg = d3
@@ -50,8 +52,7 @@ function createSvg() {
         //   100 * Math.random()) %
         //   5) +
         // 500
-        ((window.innerWidth / frequencyData.length) * i + 100 * Math.random()) %
-        1000
+        (window.innerWidth / frequencyData.length) * i + 100 * Math.random()
       );
     })
     .attr("cy", function (d, i) {
@@ -104,13 +105,35 @@ document.getElementById("file-input").onchange = function () {
   }
 };
 
-document.getElementById("demo-button").onclick = function () {
-  setDemoSong();
+document.getElementById("demo-button-1").onclick = function () {
+  setDemoSong1();
 };
 
-function setDemoSong() {
+document.getElementById("demo-button-2").onclick = function () {
+  setDemoSong2();
+};
+
+infoLink.onclick = () => {
+  modal.style.display = "";
+};
+
+document.getElementById("close-modal").onclick = () => {
+  modal.style.display = "none";
+};
+
+document.getElementById("modal").onclick = (e) => {
+  if (e.target === modal) modal.style.display = "none";
+};
+
+function setDemoSong1() {
   const src = "./assets/followurdreams.mp3";
   const trackName = "follow ur dreams <3";
+  changeSong(src, trackName);
+}
+
+function setDemoSong2() {
+  const src = "./assets/Kuku.m4a";
+  const trackName = "Kuku";
   changeSong(src, trackName);
 }
 
